b1f950803bcc57f58cf456ed0a871d5ce02cb2f6
SKY3E <bessin.raphael@gmail.com>
2023-12-19 16:36:39 -0500

    Add new sections and update styles in about page

--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,3 @@
-import Construction from "../components/construction";
 import Video from "../components/video";
 import Divider from '../components/divider';
 import Identity from "../components/identity";
@@ -13,6 +12,8 @@ const About = () => {
       </section>
       <Divider color={"white"} />
       <section className='background-section py-8 px-4 mx-8 rounded'>
+        <h2 className='text-center text-white font-extrabold text-4xl'>How did we get together?</h2>
+        <br></br>
         <p className='text-center text-white text-xl'>
           We are Team Number 14903, The SharkBots based in Mamaroneck, NY. We are a motivated, diverse and skilled group of 10 high school students from Westchester County. Though our team number has been around for a number of seasons, in many ways we are a rookie team.
         </p>
@@ -20,6 +21,10 @@ const About = () => {
         <p className='text-center text-white text-xl'>
           At the end of the 2021-2022 season, the remaining three members of 14903, Ryan, Thomas and Jack, came to the realization that it was time to expand and grow the team so they could once again be competitive and sustainable. Meanwhile, a rookie FLL team in Mamaroneck, made up of Justin, Ben, Denis, and Vincent, was finishing up an extraordinary FLL season that led them to the Houston World Championship: At the end of their season they had to decide if they were ready to move on to the new challenge of FTC. These two teams met over late summer through a mutual acquaintance and a merging of teams made sense. The FTC experience of Ryan, Thomas and Jack combined with the motivation and skill of Justin, Ben, Vincent and Denis seemed a perfect union. They were thrilled to add  Paolo, Raphael, Maia, Inaara, Charlie, Rafael and Adrien to round out the team. Though we kept our team number due to logistical reasons, we emerged as SharkBots.
         </p>
+      </section>
+      <Divider color={"white"} />
+      <section className='background-section py-8 px-4 mx-8 rounded'>
+        <h2 className='text-center text-white font-extrabold text-4xl'>How did last year go?</h2>
         <br></br>
         <p className='text-center text-white text-xl'>
           We have been given great support from the French American School in Mamaroneck to develop our young team. They renovated a space on our behalf that is large, bright and equipped with Wifi, whiteboards, tables and other necessities, and is now known as SharkBots HQ. Above all else, they did not limit team members solely to the French American School community. In fact, our 14 members represented four separate high schools in Westchester County.
@@ -32,6 +37,10 @@ const About = () => {
         <p className='text-center text-white text-xl'>
           With the merging of two teams, we had many coaches and mentors in this first year to support us and help organize our large team to which we are grateful. Thanks to them and to the dedication which all of our members showed, we were able to achieve what we thought was impossible, attend the World Championship! This journey helped us grow as a team and taught us many valuable skills which we hope to develop and expand upon in our second year of FTC.
         </p>
+      </section>
+      <Divider color={"white"} />
+      <section className='background-section py-8 px-4 mx-8 rounded'>
+        <h2 className='text-center text-white font-extrabold text-4xl'>What's planned this year?</h2>
         <br></br>
         <p className='text-center text-white text-xl'>
           In this new Centerstage season of FTC we hope to continue learning and growing as a team with our two new members Clovis and Tristan but also to stay competitive in our region while establishing the groundwork for a thriving, sustainable team for many years to come, one with a strong business model and robust outreach. 
@@ -42,7 +51,7 @@ const About = () => {
         <h2 className='text-center text-white font-extrabold text-4xl'>Take a look at our team members!</h2>
       </section>
       <Divider color={"white"} />
-      <section className="flex justify-between grid grid-cols-4 gap-x-4 gap-y-8 background-section py-8 px-8 mx-8 rounded">
+      <section className="flex grid grid-cols-4 gap-x-4 gap-y-8 background-section py-8 px-8 mx-8 rounded">
         <Identity name="Jack" job="Head Captain" photoURL="/Team/Jack.jpg" />
         <Identity name="Rafael" job="Outreach Facilitator" photoURL="/Team/Rafael.png" />
         <Identity name="Vincent" job="Building Facilitator" photoURL="/Team/Vincent.jpg" />
@@ -54,7 +63,7 @@ const About = () => {
         <Identity name="Denis" job="Programmer" photoURL="/Team/Denis.jpg" />
         <Identity name="Maia" job="Builder" photoURL="/Team/Maia.jpg" />
       </section>
-      <Construction />
+      <Divider color={"white"} />
     </article>
   );
 };
--- app/components/identity.js
+++ app/components/identity.js
@@ -2,7 +2,7 @@ const Identity = ({ name, job, photoURL }) => {
   return (
     <div>
       <img className="rounded-lg" src={photoURL} alt={name} />
-      <div className="flex justify-between bg-white mt-2 px-2 rounded">
+      <div className="flex justify-between bg-gray-600 text-white mt-2 px-2 rounded">
         <h4 className="font-bold">{name}</h4>
         <h4 className="font-bold">{job}</h4>
       </div>
