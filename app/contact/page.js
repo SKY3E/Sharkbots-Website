f62b4b764cdfdddd4eb76aff7bef72d26a0bdec5
SKY3E <bessin.raphael@gmail.com>
2023-12-24 22:28:49 -0500

    Add ContactForm component and update Contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,8 +1,27 @@
 import Construction from "../components/construction";
+import Divider from '../components/divider';
+import ContactForm from "../components/contactform";
 
 const Contact = () => {
   return (
-    <Construction />
+    <article>
+      <Divider color={"white"} />
+      <section className='background-section py-8 mx-8 rounded'>
+        <h2 className='text-center text-white font-extrabold text-4xl'>Contact Us</h2>
+      </section>
+      <Divider color={"white"} />
+      <section className='background-section flex justify-between py-8 px-8 mx-8 rounded'>
+        <h2 className='text-white font-extrabold text-4xl'>Our Links</h2>
+        <div className="flex">
+          <a target="_blank" href="https://www.instagram.com/ftc14903/?hl=en"><img className="h-8 w-8 mr-4 rounded rotate-on-hover" src="/Instagram.png" alt="Instagram" /></a>
+          <a target="_blank" href="https://www.youtube.com/@14903"><img className="h-8 w-8 mr-4 rounded rotate-on-hover" src="/Youtube.png" alt="Youtube" /></a>
+          <a href="mailto:bessin.raphael@gmail.com"><img className="h-8 w-8 rounded rotate-on-hover" src="/Mail.png" alt="Email" /></a>
+        </div>
+      </section>
+      <Divider color={"white"} />
+      <ContactForm />
+      <Divider color={"white"} />
+    </article>
   );
 };
 
