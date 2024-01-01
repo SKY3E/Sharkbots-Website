5ed2d84bec9f2daf83ef495320a459bc8c6a9552
SKY3E <bessin.raphael@gmail.com>
2024-01-01 15:56:23 -0500

    Add code optimization and fix social logo names

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Slideshow from './components/slideshow';
 import Divider from './components/divider';
+import Event from './components/event';
 
 const images = [
   '/TeamMeeting.jpg',
@@ -25,14 +26,7 @@ const Home = () => {
         <p className='text-center text-white text-xl'>We are passionate about science and technology, and our mission is to share this passion, connect with others, and ultimately learn and grow from one another.</p>
       </section>
       <Divider color={"white"} />
-      <section className='flex justify-around'>
-        <img className='object-cover height-rsection w-full mr-4 rounded-r-lg' src='/Team.jpg'></img>
-        <div className='height-rsection w-full background-section ml-4 rounded-l-lg'>
-          <h2 className='text-white font-extrabold text-4xl p-8'>Teamwork makes the dreamwork ...</h2>
-          <p className='text-white text-xl px-8 pb-2'>We are Team Number 14903, the SharkBots based in Mamaroneck, NY.</p>
-          <p className='text-white text-xl px-8'>We are a <span className='font-extrabold'>motivated, diverse and skilled group</span> of 10 high school students from all over Westchester County, who came together after the merging of two FIRST teams. Though our team number has been around for several seasons, many of our members are relatively new to FTC and in many ways we are a rookie team. <span className='font-extrabold'>We are embracing new opportunities to build and establish our team from the ground up and to be competitive and sustainable for many years to come.</span></p>
-        </div>
-      </section>
+      <Event direction="left" image="/Team.jpg" title="Teamwork makes the dreamwork ..." hook="We are Team Number 14903, the SharkBots based in Mamaroneck, NY." text="We are a motivated, diverse and skilled group of 10 high school students from all over Westchester County, who came together after the merging of two FIRST teams. Though our team number has been around for several seasons, many of our members are relatively new to FTC and in many ways we are a rookie team. We are embracing new opportunities to build and establish our team from the ground up and to be competitive and sustainable for many years to come." />
       <Divider color={"white"} />
       <section className='background-section py-8 px-4 mx-8 rounded'>
         <h2 className='text-center text-white font-extrabold text-4xl pb-8'>Recent Events : Debugging the Past, Coding the Future!</h2>
@@ -40,23 +34,9 @@ const Home = () => {
         <p className='text-center text-white text-xl'>Since September, we've met up every week to build our current robot and recently participated in the first qualifiers for our region and were the 1st Alliance Captain.</p>
       </section>
       <Divider color={"white"} />
-      <section className='flex justify-around'>
-        <div className='height-rsection w-full background-section mr-4 rounded-r-lg'>
-          <h2 className='text-white font-extrabold text-4xl p-8'>Collaboration in the game, almost clinched the fame ...</h2>
-          <p className='text-white text-xl px-8 pb-2'>Aspiring through alliances, we reached the finals on December 2nd.</p>
-          <p className='text-white text-xl px-8'>On December 2nd, we eagerly entered the <span className='font-extrabold'>inaugural regional qualifier</span>. As the day unfolded, we forged valuable connections and alliances that propelled us to the finals, where <span className='font-extrabold'>we proudly assumed the role of the 1st alliance captain</span>. Despite our best efforts, success didn't grace us with a spot in the State Qualifiers this time. <span className='font-extrabold'>Undeterred, we stand resilient and fully prepared to surmount challenges and secure our place in the next pursuit.</span></p>
-        </div>
-        <img className='object-cover height-rsection w-full ml-4 rounded-l-lg' src='/JohnJay.jpg'></img>
-      </section>
+      <Event direction="right" image="/JohnJay.jpg" title="Collaboration in the game, almost clinched the fame ..." hook="Aspiring through alliances, we reached the finals on December 2nd." text="On December 2nd, we eagerly entered the inaugural regional qualifier. As the day unfolded, we forged valuable connections and alliances that propelled us to the finals, where we proudly assumed the role of the 1st alliance captain. Despite our best efforts, success didn't grace us with a spot in the State Qualifiers this time. Undeterred, we stand resilient and fully prepared to surmount challenges and secure our place in the next pursuit." />
       <Divider color={"white"} />
-      <section className='flex justify-around'>
-        <img className='object-cover height-rsection w-full mr-4 rounded-r-lg' src='/Team.jpg'></img>
-        <div className='height-rsection w-full background-section ml-4 rounded-l-lg'>
-          <h2 className='text-white font-extrabold text-4xl p-8'>Community drives us forward ...</h2>
-          <p className='text-white text-xl px-8 pb-2'>Last weekend, we connected with our FASNY non-stem community through an Open House event.</p>
-          <p className='text-white text-xl px-8'>In early December, we participated in an <span className='font-extrabold'>Open House event</span> at our school, FASNY, engaging with prospective parents, teachers, and faculty. At our booth with our partner FLL team, <span className='font-extrabold'>we introduced our FTC team and showcased our latest robot</span>, emphasizing our engineering mindset. We had insightful discussions, sharing our team's daily endeavors and achievements with parents and are <span className='font-extrabold'>looking forward to more such events in the future!</span></p>
-        </div>
-      </section>
+      <Event direction="left" image="/Team.jpg" title="Community drives us forward ..." hook="Last weekend, we connected with our FASNY non-stem community through an Open House event." text="In early December, we participated in an Open House event at our school, FASNY, engaging with prospective parents, teachers, and faculty. At our booth with our partner FLL team, we introduced our FTC team and showcased our latest robot, emphasizing our engineering mindset. We had insightful discussions, sharing our team's daily endeavors and achievements with parents and are looking forward to more such events in the future!" />
       <Divider color={"white"} />
     </article>
   );
