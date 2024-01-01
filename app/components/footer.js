dd9f3a9b4019b650a058d06e12f22ad25f952020
SKY3E <bessin.raphael@gmail.com>
2024-01-01 16:01:47 -0500

    Fix logo link

--- app/components/footer.js
+++ app/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <h3 className="text-white text-xl font-extrabold">© 2023 Sharkbots</h3>
         <div className="flex">
           <a target="_blank" href="https://www.instagram.com/ftc14903/?hl=en"><img className="h-8 w-8 mr-4 rounded rotate-on-hover" src="/Logos/Instagram.png" alt="Instagram" /></a>
-          <a target="_blank" href="https://www.youtube.com/@14903"><img className="h-8 w-8 mr-4 rounded rotate-on-hover" src="/logos/Youtube.png" alt="Youtube" /></a>
+          <a target="_blank" href="https://www.youtube.com/@14903"><img className="h-8 w-8 mr-4 rounded rotate-on-hover" src="/Logos/Youtube.png" alt="Youtube" /></a>
           <a href="mailto:Ftc.sharkbots@gmail.com"><img className="h-8 w-8 rounded rotate-on-hover" src="/Logos/Mail.png" alt="Email" /></a>
         </div>
       </div>
