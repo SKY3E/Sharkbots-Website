378d915ad240bf7fa834e275666155c2bade08e4
SKY3E <bessin.raphael@gmail.com>
2024-01-02 11:28:54 -0500

    Fix responsive navbar features

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -25,10 +25,10 @@ const Navbar = () => {
           </svg>
         </button>
         {showMenu && (
-          <div className="flex flex-col absolute top-20 right-4 w-1/2 md:w-1/3 mt-2 p-4 rounded shadow-lg background-section">       
+          <div className="flex flex-col absolute top-20 right-4 w-4/6 md:w-1/3 mt-2 p-4 rounded shadow-lg background-section">       
             <h3 className='text-right mb-2'>Past Seasons</h3>
             <a href="/about" className="text-sm underline-animation md:hidden">About</a>
-            <a href="/powerplay" className="text-sm underline-animation mb-1">2022-2023 Powerplay</a>
+            <a href="/powerplay" className="text-sm underline-animation">2022-2023 Powerplay</a>
             <a href="/centerstage" className="text-sm underline-animation md:hidden">2023-2024 Centerstage</a>
             <a href="/sponsors" className="text-sm underline-animation md:hidden">Sponsors</a>
             <a href="/contact" className="text-sm underline-animation md:hidden">Contact</a>
