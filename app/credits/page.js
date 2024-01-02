3c484081811d075b22f1f17710a4448e74d4b3ea
SKY3E <bessin.raphael@gmail.com>
2024-01-02 11:33:13 -0500

    Update credits text sizes

--- app/credits/page.js
+++ app/credits/page.js
@@ -11,8 +11,8 @@ const Credits = () => {
       <section className='background-section py-8 px-4 mx-8 rounded text-white'>
         <h2 className='text-white font-extrabold text-4xl'>Icon Scout</h2>
         <br></br>
-        <div className='flex items-center gap-x-1 text-xl mb-2'><img className="h-8 w-8 mr-4 rounded rotate-on-hover" src="/Logos/Youtube.png" alt="Youtube" /><a href="https://iconscout.com/icons/internet" class="text-underline font-size-sm" target="_blank">Youtube</a> by <a href="https://iconscout.com/contributors/iconscout" class="text-underline font-size-sm">IconScout Store</a></div>
-        <div className='flex items-center gap-x-1 text-xl'><img className="h-8 w-8 mr-4 rounded rotate-on-hover" src="/Logos/Instagram.png" alt="Instagram" /><a href="https://iconscout.com/icons/chatting" class="text-underline font-size-sm" target="_blank">Instagram</a> by <a href="https://iconscout.com/contributors/iconscout" class="text-underline font-size-sm" target="_blank">IconScout Store</a></div>
+        <div className='flex items-center gap-x-1 text-xs font-bold sm:text-xl mb-2'><img className="h-8 w-8 mr-4 rounded rotate-on-hover" src="/Logos/Youtube.png" alt="Youtube" /><a href="https://iconscout.com/icons/internet" class="text-underline font-size-sm" target="_blank">Youtube</a> by <a href="https://iconscout.com/contributors/iconscout" class="text-underline font-size-sm">IconScout Store</a></div>
+        <div className='flex items-center gap-x-1 text-xs font-bold sm:text-xl'><img className="h-8 w-8 mr-4 rounded rotate-on-hover" src="/Logos/Instagram.png" alt="Instagram" /><a href="https://iconscout.com/icons/chatting" class="text-underline font-size-sm" target="_blank">Instagram</a> by <a href="https://iconscout.com/contributors/iconscout" class="text-underline font-size-sm" target="_blank">IconScout Store</a></div>
       </section>
       <Divider color={"white"} />
       <section className='background-section py-8 px-4 mx-8 rounded text-white'>
